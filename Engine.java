482d8abaebefc26832401e9a3f0be3d3ead7db21
David Gipson <david.gipson@utexas.edu>
2020-02-13 13:06:48 -0600

    Added checkTie feature

--- Engine.java
+++ Engine.java
@@ -59,7 +59,14 @@ public class Engine {
   // Return true if the board is full
   // Return false if not
   public boolean checkTie() {
-    return true;
+	for (int i = 0; i < BOARD_SIZE; i++) {
+		for (int j = 0; j < BOARD_SIZE; j++) {
+			if (board[i][j].equals(" ")) {
+				return false;
+			}
+		}
+	}
+	return true;
   }
 
   public int checkWinner() {
